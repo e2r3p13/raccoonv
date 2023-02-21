4c575a1f25cbbe11356713e6d81747e8c182f942
lfalkau <lfalkau@student.42.fr>
2023-02-21 18:47:26 +0100

    [capstone] Find gadget roots based on an instruction set
    Implements the find_gadget_roots function, that returns a vector of
    offsets at which an instruction of interest is present in the given
    code.
    
    In this function,the capstone engine performs a first pass on the
    given code, disassembling one isntruction at each bound-aligned offset
    (using the ALIGNMENT constant).
    If this instruction is present if the set of instructions of interest,
    its offset is pushed to a vector returned by the function.
    
    Signed-off-by: lfalkau <lfalkau@student.42.fr>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,51 @@
 use capstone::prelude::*;
+use capstone::arch::riscv::RiscVInsn;
 use std::iter;
+
+const ALIGNMENT: usize = 2;
+
+const BRANCH_INSNS: &[RiscVInsn] = &[
+    RiscVInsn::RISCV_INS_JAL,
+    RiscVInsn::RISCV_INS_JALR,
+    RiscVInsn::RISCV_INS_C_JAL,
+    RiscVInsn::RISCV_INS_C_JALR,
+    RiscVInsn::RISCV_INS_MRET,
+    RiscVInsn::RISCV_INS_SRET,
+    RiscVInsn::RISCV_INS_URET,
+];
+
+fn find_gadget_roots(cs: &capstone::Capstone, code: &[u8]) -> Vec<usize> {
+    let mut roots = Vec::new();
+
+    for off in (0..code.len() + ALIGNMENT).step_by(ALIGNMENT) {
+        if let Ok(insns) = cs.disasm_count(&code[off..], (0x1010c + off) as u64, 1) {
+            if let Some(ins) = insns.first() {
+                if BRANCH_INSNS.contains(&RiscVInsn::from(ins.id().0)) {
+                    roots.push(off + ins.len());
+                }
+            }
+        }
+    }
+    return roots;
+}
 
 fn main() {
     // RISC-V code with some branching instructions (C extension)
     let code: &[u8] = b"\x05\x45\x93\x08\xd0\x05\x73\x00\x00\x00\x82\x90\x02\x94\x82\x93\x02\x95\x67\x80\x00\x00";
 
-    let cs = Capstone::new()
+    let mut cs = Capstone::new()
         .riscv()
         .mode(arch::riscv::ArchMode::RiscV64)
-        .detail(true)
         .extra_mode(iter::once(arch::riscv::ArchExtraMode::RiscVC))
         .build()
         .expect("Failed to create Capstone object");
 
-    let insns = cs.disasm_all(code, 0x1000)
-        .expect("Failed to disassemble code");
+    cs.set_detail(false).expect("Failed to update Capstone object");
+    let gadget_roots = find_gadget_roots(&cs, &code);
 
-    println!("Successfully disassembled {} instruction(s)", insns.len());
-    for ins in insns.as_ref() {
-        println!("i: {ins}");
+    cs.set_detail(true).expect("Failed to update Capstone object");
+    for root in gadget_roots {
+        // TODO: Disassemble backward and print gadgets
     }
+
 }
